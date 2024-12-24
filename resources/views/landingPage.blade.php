a637f0326d8c4a596cf9bca97355edf97eacd2ae
robinmahz <robinmaharjan25@gmail.com>
2024-12-24 22:19:35 +0545

    init

---
+++ resources/views/landingPage.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Note Share</title>
+    @vite('resources/css/app.css')
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+
+<body>
+    @include('components.frontend.navbar')
+    @include('components.frontend.footer')
+</body>
+
+</html>
